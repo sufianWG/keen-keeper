3e6ea417386c4555bb5acf520aa5f36cd8df659f
sufianWG <webgurumrmbd@gmail.com>
2026-06-19 21:25:39 +0600

    created, designed and made responsive the navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,24 +1,34 @@
+import { Link, NavLink } from "react-router";
+import logoImg from "../../../assets/logo.png"
+import { RiHome2Line, RiTimeLine } from "react-icons/ri";
+import { FaChartLine } from "react-icons/fa";
+import { IoClose, IoMenu } from "react-icons/io5";
+import { useState } from "react";
+
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const navLinkClass = ({isActive}) => `py-3 px-4 text-base flex items-center gap-1 hover:bg-[#244D3F] hover:rounded-md hover:text-white hover:font-bold transition-all duration-500 ${isActive ? "bg-[#244D3F] text-white rounded-md": ""}`;
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+        <nav>
+            <div className="md:flex md:justify-between items-center bg-white py-4.5 px-10 md:px-20 shadow">
+                <div className="leftSide">
+                    <Link to={"/"}><img src={logoImg} alt="logo" /></Link> 
+                </div>
+                <div className="text-3xl absolute right-10 top-5 cursor-pointer md:hidden" onClick={() => setOpen(!open)}>
+                    {
+                        open ? <IoClose /> : <IoMenu />
+                    }
+                    
+                </div>
+                <div className={`rightSide flex-none md:flex absolute md:static bg-white w-full md:w-auto left-0 md:z-auto transition-all duration-300 ease-in ${ open? "top-18" : "top-[-500px]"}`}>
+                    <NavLink to={"/"} className={navLinkClass} onClick={()=> setOpen(false)}><RiHome2Line /> Home</NavLink>
+                    <NavLink to={"/timeline"} className={navLinkClass} onClick={()=> setOpen(false)}> <RiTimeLine /> Timeline</NavLink>
+                    <NavLink to={"/stats"} className={navLinkClass} onClick={()=> setOpen(false)}> <FaChartLine /> Stats</NavLink>
+                </div>
+
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </nav>
     );
 };
 
