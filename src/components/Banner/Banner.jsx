acdc374fdd8104dec4c60191fe6b142ac9fc9a4d
sufianWG <webgurumrmbd@gmail.com>
2026-06-20 17:37:57 +0600

    designed the banner section with responsive

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,37 @@
+import { FaPlus } from "react-icons/fa";
+
+const Banner = () => {
+    return (
+        <div className="bg-[#F8FAFC]">
+            <div className="w-9/12 mx-auto pt-20 pb-10">
+                <div className="bannerInfo space-y-8 text-center pb-10">
+                    <h1 className="text-5xl font-bold ">Friends to keep close in your life</h1>
+                    <p className="text-base text-[#64748B] ">Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                        relationships that matter most.</p>
+                    <button className="py-3 px-4 text-white font-semibold rounded-md bg-[#244D3F] flex items-center gap-1 mx-auto cursor-pointer"> <FaPlus /> Add a Friend</button>
+                </div>
+                <div className="infoCards grid grid-cols-1 md:grid-cols-4 gap-6">
+                    <div className="text-center bg-white p-8 rounded-md shadow">
+                        <h3 className="font-semibold text-3xl text-[#244D3F]">10</h3>
+                        <p className="text-[1.125rem] text-[#64748B]">Total Friends</p>
+                    </div>
+                    <div className="text-center bg-white p-8 rounded-md shadow">
+                        <h3 className="font-semibold text-3xl text-[#244D3F]">3</h3>
+                        <p className="text-[1.125rem] text-[#64748B]">On Track</p>
+                    </div>
+                    <div className="text-center bg-white p-8 rounded-md shadow">
+                        <h3 className="font-semibold text-3xl text-[#244D3F]">6</h3>
+                        <p className="text-[1.125rem] text-[#64748B]">Need Attention</p>
+                    </div>
+                    <div className="text-center bg-white p-8 rounded-md shadow">
+                        <h3 className="font-semibold text-3xl text-[#244D3F]">12</h3>
+                        <p className="text-[1.125rem] text-[#64748B]">Interactions This Month</p>
+                    </div>
+                </div>
+                <hr className="mt-10 text-gray-200" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../components/Banner/Banner";
+
 const Home = () => {
     return (
         <div>
-            Homepage            
+            <Banner></Banner>            
         </div>
     );
 };
